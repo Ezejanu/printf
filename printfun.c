19c5164a3c19fed7a9c90c25a30c8866dafd061c
Ezejanu <Ezejanu_nno@ymail.com>
2023-05-29 09:11:10 -0700

    corrections

--- printfun.c
+++ printfun.c
@@ -1,25 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
-
 /**
 * _printf - a function to perform the same functions as printf
 * @format: a character string composed of zero or more directives.
 * Return: Always (0)
 */
-
 int _printf(const char *format, ...)
 {
 	unsigned int a, count = 0, string;
 	va_list toprint;
 
 	va_start(toprint, format);
-
 	for (a = 0; format[a] != '\0'; a++)
 	{
 		if (format[a] != '%')
 			_putchar(format[a]);
-
 		else if (format[a + 1] == 'c')
 		{
 			_putchar(va_arg(toprint, int));
@@ -41,12 +37,14 @@ int _printf(const char *format, ...)
 			getint(va_arg(toprint, int));
 			a++;
 		}
-		else
-		{
-			_putchar('%');
-			_putchar(format[a + 1]);
-			a++;
-		}
+	/**
+	*	else
+	*	{
+	*		_putchar('%');
+	*		_putchar(format[a + 1]);
+	*		a++;
+	*	}
+	*/
 		count++;
 	}
 	return (count);
